37832d95cf6d02cb6318830edc6497ff5a04ec13
Stefanyb918 <gloria.bohorquez.se@bvc.com.co>
2023-03-23 19:40:24 -0500

    solucion error2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'https://stefanyb918.github.io/gb-react-vite/',
-  plugins: [react()],
+  plugins: [react()]
 })
